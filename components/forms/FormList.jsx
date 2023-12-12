7f3ff3511d7f7a13ae77b274fd4f9fa68dbabd7e
Bora Lee <brkkie13@naver.com>
2023-12-12 18:03:56 +0900

    Feat : FormList table 반응형 작업, 날짜 표기 함수 추가

--- components/forms/FormList.jsx
+++ components/forms/FormList.jsx
@@ -3,36 +3,33 @@ import { FormListStyled } from './FormList.styles';
 import { LinkIcon, CopyIcon } from '../../\bstyles/Icons';
 import { IconButtonStyled } from '../ui/Button.styles';
 import Tooltip from '../ui/Tooltip';
+import { formatDate } from '../../utils/date';
 
 // code
 function FormList({ currentPosts, onShow, onCopy }) {
   return (
     <FormListStyled>
-      <thead>
-        <tr>
-          <th>제목</th>
-          <th>생성 날짜</th>
-        </tr>
-      </thead>
       <tbody>
         {currentPosts.map(data => (
           <tr key={data.id} onClick={() => onShow(data.id)}>
-            <td>{data.header}</td>
+            <td>
+              <div className="header">{data.header}</div>
+              <div className="date-and-controls">
+                <span className="date">{formatDate(data.creationDate)}</span>
+                <span className="controls">
+                  <Tooltip text="링크 복사">
+                    <IconButtonStyled>
+                      <LinkIcon />
+                    </IconButtonStyled>
+                  </Tooltip>
 
-            <td>{new Date(data.creationDate).toLocaleString()}</td>
-
-            <td className="controls">
-              <Tooltip text="링크 복사">
-                <IconButtonStyled>
-                  <LinkIcon />
-                </IconButtonStyled>
-              </Tooltip>
-
-              <Tooltip text="폼 복제">
-                <IconButtonStyled onClick={event => onCopy(event, data.id)}>
-                  <CopyIcon />
-                </IconButtonStyled>
-              </Tooltip>
+                  <Tooltip text="폼 복제">
+                    <IconButtonStyled onClick={event => onCopy(event, data.id)}>
+                      <CopyIcon />
+                    </IconButtonStyled>
+                  </Tooltip>
+                </span>
+              </div>
             </td>
           </tr>
         ))}
