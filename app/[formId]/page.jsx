81adf017b9c26c4904ff1935f8cd3f6ccd2f1c1e
Bora Lee <brkkie13@naver.com>
2024-01-02 12:50:10 +0900

    Fix : form id가 유효하지 않거나 localData로 시작할 때(로그인을 안하고 작성한 임시 게시물) 알림 페이지 삽입

--- app/[formId]/page.jsx
+++ app/[formId]/page.jsx
@@ -7,6 +7,8 @@ import { Section, SectionCard } from '@components/ui/Section';
 // redux
 import { useDispatch } from 'react-redux';
 import { fetchFormDataWithFormId } from '@stores/actions/formActionCreators';
+import NotificationBanner from '@/components/ui/NotificationBanner';
+import { CautionIcon } from '@/components/assets/Icons';
 
 // code
 function SharedFormDetailPage() {
@@ -20,6 +22,21 @@ function SharedFormDetailPage() {
       setForm(formData)
     );
   }, [dispatch, formId]);
+
+  // form id가 유효하지 않거나 localData로 시작할 때(로그인을 안하고 작성해서 브라우저에 임시로 저장된 게시물) 접근 불가
+  if (formId.startsWith('localData') || !form) {
+    return (
+      <Section>
+        <SectionCard>
+          <NotificationBanner
+            icon={<CautionIcon />}
+            mainText={'존재하지 않는 폼 입니다.'}
+            subText={'올바른 주소로 접근해주세요.'}
+          />
+        </SectionCard>
+      </Section>
+    );
+  }
 
   return (
     <Section>
